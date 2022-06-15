66a131d7fe8dab37723ffc676428b1c63c6283d1
chuducdoan <48355044+Ongnoiban2010@users.noreply.github.com>
2022-06-15 20:30:32 +0700

    done

--- App/tiktok/src/App.js
+++ App/tiktok/src/App.js
@@ -46,6 +46,12 @@ function App() {
   const [checked, setChecked] = useState();
   const [checkbox, setCheckbox] = useState([]);
 
+  const [job, setJob] = useState('');
+  const [jobs, setJobs] = useState(() => {
+    const storageJob = JSON.parse(localStorage.getItem('jobs')) ?? []; // dung ?? thay vi || vi biet chac ket qua qua tra ve null hoac undifine
+    return storageJob;
+  });
+
   const handleIncrease = () => {
     setCounter(counter + 1);
     // setInfo({
@@ -83,21 +89,32 @@ function App() {
     });
   }
 
-  console.log(checkbox)
+  const handleAddJob= () => {
+    setJobs(prev => {
+      const newJob = [...prev, job];
+      localStorage.setItem('jobs', JSON.stringify(newJob));
+      console.log(JSON.stringify(newJob));
+      return newJob;
+    }); // goi ham len lich trinh khi nao render lai App thi jobs moi nhan gia tri moi
+    setJob('');
+    console.log(jobs)
+  }
 
   return (
     <div className="App">
-      <h1>{counter}</h1>
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <h2>{gift || 'Chưa có phần thưởng'}</h2>
-      <button onClick={randomGift}>Lấy thưởng</button>
-      <br></br>
-      <input  type="text" onChange={e => setName(e.target.value)} value={name}/>
-      <br/>
-      <input type="text" onChange={e => setEmail(e.target.value)}  value={email}/>
-      <br/>
-      <button onClick={handleSubmit}>Change</button>
+      <div>
+        <h1>{counter}</h1>
+        <h1>{JSON.stringify(info)}</h1>
+        <button onClick={handleIncrease}>Increase</button>
+        <h2>{gift || 'Chưa có phần thưởng'}</h2>
+        <button onClick={randomGift}>Lấy thưởng</button>
+        <br></br>
+        <input  type="text" onChange={e => setName(e.target.value)} value={name}/>
+        <br/>
+        <input type="text" onChange={e => setEmail(e.target.value)}  value={email}/>
+        <br/>
+        <button onClick={handleSubmit}>Change</button>
+      </div>
       <hr/>
       {cource.map((sour) => 
         <div key={sour.id}>
@@ -116,6 +133,14 @@ function App() {
           {sour.name}
         </div>
       )}
+      <hr/>
+      <input type="text" onChange={(e) => setJob(e.target.value)} value={job}/>
+      <button onClick={handleAddJob}>Add</button>
+      <ul style={{width: '200px', margin: '0 auto'}}>
+        {jobs.map((value, index) => 
+          <li key={index}>{value}</li>
+        )}
+      </ul>
     </div>
   );
 }
