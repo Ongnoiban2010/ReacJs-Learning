743407c704047466e280b7103def7fafb3f98275
chuducdoan <48355044+Ongnoiban2010@users.noreply.github.com>
2022-06-16 22:09:48 +0700

    add

--- App/tiktok/src/Content.js
+++ App/tiktok/src/Content.js
@@ -7,16 +7,38 @@ function Content() {
     const [title, setTitle] = useState('');
     const [posts, setPosts] = useState([]);
     const [type, setType] = useState('posts');
+    const [showGoToTop, setShowGoToTop] = useState(false);
 
     useEffect(() => {
         document.title = title;
-        // fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        // .then(res => res.json())
-        // .then(posts => {
-        //     setPosts(posts);
-        // })
-        console.log('re-rendere')
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(res => res.json())
+        .then(posts => {
+            setPosts(posts);
+        })
     }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 200) {
+                setShowGoToTop(true)
+                console.log('state')
+            } else {
+                setShowGoToTop(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        console.log('add listener')
+
+        // cleanup function
+        // dc goi truoc khi component do bi unmount
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            console.log('remove listener')
+        }
+    }, [])
+
+    console.log('re-render')
 
     return (
         <div>
@@ -36,6 +58,10 @@ function Content() {
                     <li key={post.id}>{post.title}</li>
                 ))}
             </ul>
+            {showGoToTop && (
+                <button style={{position: 'fixed', bottom: '10px', right: '10px'}}>Go to top</button>
+            )
+            }
         </div>
     )
 }
