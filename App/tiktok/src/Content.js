b9e8c5679a994d94ea1656128dd4d9aef9ea15e3
chuducdoan <48355044+Ongnoiban2010@users.noreply.github.com>
2022-06-16 21:34:28 +0700

    add

--- App/tiktok/src/Content.js
+++ App/tiktok/src/Content.js
@@ -1,21 +1,32 @@
 import {useEffect, useState} from 'react';
 
+var tabs = ['posts', 'todos', 'albums'];
+
 function Content() {
 
     const [title, setTitle] = useState('');
     const [posts, setPosts] = useState([]);
+    const [type, setType] = useState('posts');
 
     useEffect(() => {
         document.title = title;
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(post => {
-            setPosts(post);
-        })
+        // fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        // .then(res => res.json())
+        // .then(posts => {
+        //     setPosts(posts);
+        // })
+        console.log('re-rendere')
     }, [])
 
     return (
         <div>
+            {tabs.map(tab => (
+                <button key={tab} 
+                style={type === tab ? {backgroundColor: 'red'} : {}}
+                onClick={() => setType(tab)}
+                >{tab}</button>
+            ))}
+            <br></br>
             <input
                 value={title} 
                 onChange={(e) => setTitle(e.target.value)}
