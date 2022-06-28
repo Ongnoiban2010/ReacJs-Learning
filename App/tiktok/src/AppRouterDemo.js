84a0fded84442f2454cd66c95ba5b510db8827d4
chuducdoan <48355044+Ongnoiban2010@users.noreply.github.com>
2022-06-28 20:22:14 +0700

    add

--- App/tiktok/src/AppRouterDemo.js
+++ App/tiktok/src/AppRouterDemo.js
@@ -1,31 +1,48 @@
-import {Routes, Route, Outlet, Link} from 'react-router-dom'
+import { Routes, Route, Outlet, Link } from 'react-router-dom'
 import HomePage from './pages/Home'
 import NewPage from './pages/New'
 import ContactPage from './pages/Contact'
 
 function AppRouterDemo() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/news">News</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
+    return ( <
+        div >
+        <
+        nav >
+        <
+        ul >
+        <
+        li >
+        <
+        Link to = "/" > Home < /Link> <
+        /li> <
+        li >
+        <
+        Link to = "/news" > News < /Link> <
+        /li> <
+        li >
+        <
+        Link to = "/contact" > Contact < /Link> <
+        /li> <
+        /ul> <
+        /nav>
 
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/news" element={<NewPage/>} />
-                <Route path="/contact" element={<ContactPage/>} />
-            </Routes>
-        </div>
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < HomePage / > }
+        /> <
+        Route path = "/news"
+        element = { < NewPage / > }
+        /> <
+        Route path = "/contact"
+        element = { < ContactPage / > }
+        /> <
+        Route path = "/admin"
+        element = { < AdminPage / > }
+        /> <
+        /Routes> <
+        /div>
     )
 }
 
