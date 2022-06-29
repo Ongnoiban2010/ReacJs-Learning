b5f6d59e50eb4b9c15fda7f3300b8d923b2d074c
chuducdoan <48355044+Ongnoiban2010@users.noreply.github.com>
2022-06-29 21:28:38 +0700

    commit

--- App/tiktok-ui/src/App.js
+++ App/tiktok-ui/src/App.js
@@ -1,9 +1,27 @@
+import {Fragment} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {publicRoutes} from '~/routes'
+import {DefaultLayout} from '~/components/Layout'
 
 function App() {
     return (
-        <div className="App">
-           <h1>Hello World</h1>
-        </div>
+        <Router>
+            <div className='App'>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout
+                        } else if (route.layout === null) {
+                            Layout = Fragment
+                        }
+                        // biến dùng trong jsx phải viết hoa
+                        const Page = route.component
+                        return <Route key={index} path={route.path} element={<Layout><Page/></Layout>}/>
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
